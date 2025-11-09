4b7eb5f73fd79aa7c4527a1a0d3258e9a04a223f
Evgeny Volkov <zhen3100@gmail.com>
2025-11-09 23:40:14 +0300

    SpringApplication

---
+++ SpringApplication/src/main/java/com/evgeniy/spring/springapplication/DTO/UserResponse.java
@@ -0,0 +1,21 @@
+package com.evgeniy.spring.springapplication.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private Long id;
+    private String name;
+    private String email;
+    private Integer age;
+    private LocalDateTime createdAt;
+
+}
