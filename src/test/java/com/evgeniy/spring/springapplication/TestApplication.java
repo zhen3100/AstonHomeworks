4d7498aec1c7b0ecb918da0ba6db9277b0744b28
Evgeny Volkov <zhen3100@gmail.com>
2025-11-09 23:15:47 +0300

    v1

---
+++ src/test/java/com/evgeniy/spring/springapplication/TestApplication.java
@@ -0,0 +1,11 @@
+package com.evgeniy.spring.springapplication;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
